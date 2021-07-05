04828ae91afa537911d0da746815a377a91d8fd9
unknown <md6907179@gmail.com>
2021-07-05 20:06:24 -0300

    Put e Delet da dela de cadastrar temas comcluídos

--- src/app/model/UserLogin.ts
+++ src/app/model/UserLogin.ts
@@ -2,8 +2,8 @@
 export class UserLogin {
     public id: number
     public nome: string
-    public usuario: string
     public senha: string
+    public usuario: string
     public token: string
     public foto: string
     public tipo: string
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { observable, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
 
@@ -16,12 +16,31 @@ export class TemaService {
     headers: new HttpHeaders().set('Authorization',environment.token)
   }
 
+  refreshToken () {
+    this.token = {
+      headers: new HttpHeaders().set('Authorization',environment.token)
+    }
+  }
 
   getAllTema(): Observable<Tema[]>{
     return this.http.get<Tema[]>('https://blogmdrs.herokuapp.com/tema', this.token)
   }
 
+  getById(id: number): Observable<Tema> {
+    return this.http.get<Tema>(`https://blogmdrs.herokuapp.com/tema/${id}`, this.token)
+  }
+
   potTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogmdrs.herokuapp.com/tema', tema, this.token)
   }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogmdrs.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://blogmdrs.herokuapp.com/tema/${id}`, this.token)
+  }
+
+
 }
